e8b87bdd850bcce11f0b7c589642f371491cf45a
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-27 15:53:26 -0700

    Add the final function for qu
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,41 +7,41 @@ function noInputtedWord() {
   return false;
 }
 
-function vowels(string) {
-  const stringArr = string.toLowerCase().split("");
-  if (stringArr[0] === "a" || stringArr[0] === "e" || stringArr[0] === "i" || stringArr[0] === "o" || stringArr[0] === "u") {
-    return (string.concat("way"));
-
-  }
-  else {
-    for ( let i = 0; i < string.length; i++) {
-      if (["a", "e", "i", "o", "u"].indexOf(string[i]) > -1 ) {
-        let first = string.slice(0, i);
-        let middle = string.slice(i, string.length);
-        string = middle + first + "ay";
-        return string;
+function consonant (string) {
+  for ( let i = 0; i < string.length; i++) {
+    if (["a", "e", "i", "o", "u"].indexOf(string[i]) > -1 ) {
+      let first = string.slice(0, i);
+      let middle = string.slice(i, string.length);
+      string = middle + first ;
+      stringArr = string.split("");
+      if (stringArr[stringArr.length - 1] === "q" && stringArr[0] === "u") {
+        stringArr.push(stringArr[0])
+        stringArr.shift();
+        stringArr.join();
+        return stringArr.join("") + "ay";
       }
+      return  string + "ay"
     }
   }
 }
 
-for ( let i = 0; i < stringArr.length; i++) {
-  if !(stringArr[i] === "a" || stringArr[i] === "e" || stringArr[i] === "i" || stringArr[i] === "o" || stringArr[i] === "u")
 
-// for ( let i = 0; i < string.length; i++) {
-//   if ( string[i].includes("qu")) {
-//     var first = string.slice(0, i);
-//     var middle = string.slice(i , string.length);
-//     string = middle + first ;
-//   }
-// return string + "ay";
-// }
+function pigLatin(string) {
+  const stringArr = string.toLowerCase().split("");
+  if (stringArr[0] === "a" || stringArr[0] === "e" || stringArr[0] === "i" || stringArr[0] === "o" || stringArr[0] === "u") {
+    return (string.concat("way"));
+  }
+  else {
+    return consonant(string);
+  }
+}
 
-// function quMove(string) {
-//   for ( let i = 0; i < string.length; i++) {
-//       if (string.includes("qu")){
-//           string = string.substring(i) + string.substring(0,i);
-//       }
-//   }
-//   return string + "ay";
-// }
+function quMove(string) {
+  for (let i = 0; i < string.length; i++) {
+      if (string.includes("qu")){
+          string = string.substring(i) + string.substring(0,i);
+          console.log(string);
+      }                                          
+  }
+  return string;
+}
